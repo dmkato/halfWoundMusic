78cf34887649b273947e03da3e57cec5015fb882
dmkato <dmkato@gmail.com>
2017-04-20 12:58:19 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "php/header.php"; ?>
+
+<body>
+
+
+</body>
+
+</html>
---
+++ php/header.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+
+  <script src="/js/main.js"></script>
+  <script src="/lib/jquery.min.js"></script>
+  <link rel="stylesheet" href="/styles/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+
+</head>
+<header class="ShopName">
+
+  <!-- Insert svg with parallax effect-->
+  <h1>Halfwound Music</h1>
+
+</header>
+<nav class="nav">
+
+  <div class="navItem"><a href="index.php">Home</a></div>
+  <div class="navItem"><a href="#">Shop</a></div>
+  <div class="navItem"><a href="#">Account</a></div>
+  <div class="navItem"><a href="#">Cart</a></div>
+  <div class="accountItem">Welcome [Insert User Name]</div>
+
+</nav>
