12f40d5f4e02ee217dce8e478d3ff23cbeaab94e
dmkato <dmkato@gmail.com>
2017-04-22 14:49:54 -0700

    Started menu

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php include "php/header.php"; ?>
 
-<body>
-
-
 </body>
 
 </html>
--- php/header.php
+++ php/header.php
@@ -7,18 +7,29 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
 
 </head>
-<header class="ShopName">
+<header>
 
   <!-- Insert svg with parallax effect-->
-  <h1>Halfwound Music</h1>
+  <nav class="nav">
+    <h1 class="shopName">Halfwound Music</h1>
+
+    <div class="middleMenus">
+      <div class="searchBar">
+        <input type="text"></input>
+      </div>
+      <div class="menuBar">
+        <div class="navItem"><a href="index.php">Home</a></div>
+        <div class="navItem"><a href="#">Shop</a></div>
+        <div class="navItem"><a href="#">Account</a></div>
+        <div class="navItem"><a href="#">Cart</a></div>
+        <div class="navItem"><a href="#">About Us</a></div>
+      </div>
+    </div>
+
+    <div class="accountBar">
+      <div class="accountItem">Welcome [Insert User Name]</div>
+    </div>
+
+  </nav>
 
 </header>
-<nav class="nav">
-
-  <div class="navItem"><a href="index.php">Home</a></div>
-  <div class="navItem"><a href="#">Shop</a></div>
-  <div class="navItem"><a href="#">Account</a></div>
-  <div class="navItem"><a href="#">Cart</a></div>
-  <div class="accountItem">Welcome [Insert User Name]</div>
-
-</nav>
