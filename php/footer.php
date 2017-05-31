048fd05554e7585c4045ce4f06370f80974d9e25
giovanmi <giovanmi@oregonstate.edu>
2017-05-31 12:37:33 -0700

    footer now displays email when logged in

--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,21 @@
+<?php
+session_start();
+if($_SESSION["username"] == "Not logged in") {
+  $username = "";
+  
+}
+else {
+  $username = $_SESSION["username"];
+  
+}
+
+?>
+
 </body>
 <footer>
   <div class="container navbar navbar-fixed-bottom">
       <ul class="list-inline text-center">
-        <li>[User name]</li>
+        <li><?php echo "$username"; ?></li>
         <li><a href="#">Legal</a></li>
       </ul>
   </div>
