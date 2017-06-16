0ae68ad06b0a7df8a32ab6637664877ffd47eaa7
giovanmi <giovanmi@oregonstate.edu>
2017-06-15 20:19:57 -0700

    added about page, removed passwords

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## A musical instrument store for CS 340
 
+Created by Michael Giovannoni and Daniel Kato
+
 Uses php, mySQL, html, and js
+
+Setup instructions:
+
+1. download and add files to a server with PHP 5 installed.
+2. edit config.php with the correct database credentials. 
+3. edit the $directory variable in header.php and footer.php so that the links work correctly. 
--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,16 @@
+<?php
+
+//edit to correct path depending on where files are hosted
+// $directory = "/~katod/halfWoundMusic";
+
+$directory = "/~giovanmi/dbs/halfWoundMusic";
+
+?>
 </body>
 <footer>
   <div class="container navbar navbar-fixed-bottom">
       <ul class="list-inline text-center">
-        <li>Halfwound Music</li>
+        <li ><a href="<?php echo $directory;?>/php/about.php">About Halfwound Music</a></li>
         <li><a href="#">All Rights Reserved</a></li>
       </ul>
   </div>
